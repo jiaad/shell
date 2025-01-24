f27b2f49f1532826bd53cdec9a6f0fbf867f7ffa
jiaad <abdulziad08@outlook.com>
2025-01-24 21:03:55 +0100

    cd builtin

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -3,31 +3,9 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include "../commands.h"
 
 
-char *command_concat2(char *s1, char *s2){
-  char *res;
-  int i = 0;
-  int j = 0;
-  int s1_len = strlen(s1);
-  int s2_len = strlen(s2);
-  res = malloc(sizeof(char) * (s1_len + s2_len + 1));
-  if(res == NULL){
-    fprintf(stderr, "RES could not be allocated, cannot concatenate\n");
-    exit(EXIT_FAILURE);
-  }
-
-  while(i < s1_len){
-    res[i] = s1[i];
-    i++;
-  }
-  while(j < s2_len){
-    res[i++] = s2[j];
-    j++;
-  }
-  res[s2_len + s1_len] = '\0';
-  return res;
-}
 
 int my_cd(int len, char *args[]){
   printf("argc - %d\n", len);
@@ -38,7 +16,7 @@ int my_cd(int len, char *args[]){
   char buf[500];
   if (len == 2){
     getcwd(buf, 499);
-    char *comm = command_concat2(buf, (char*)args[1]);
+    char *comm = command_concat(buf, (char*)args[1]);
     printf("TODO: ADD GOTO ROOT: %s\n", comm);
     int res = chdir((char *)args[1]);
     if(res == 0){
